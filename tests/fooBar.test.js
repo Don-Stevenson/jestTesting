740f05fe012a33f74a1b14ff236fbe17c41cd6ad
Don Stevenson <dstevenson@tribalscale.com>
2022-10-25 21:26:58 -0400

    refactor to implicit return

--- tests/fooBar.test.js
+++ tests/fooBar.test.js
@@ -1,9 +1,7 @@
 const fooBar = require("../fooBar")
 
-test("expect foo to when passed in bar", () => {
-  expect(fooBar("foo")).toBe("bar")
-})
+test("expect foo to when passed in bar", () =>
+  expect(fooBar("foo")).toBe("bar"))
 
-test("expect bar to when passed in foo", () => {
-  expect(fooBar("bar")).toBe("foo")
-})
+test("expect bar to when passed in foo", () =>
+  expect(fooBar("bar")).toBe("foo"))
